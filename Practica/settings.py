328c6048cedd52d8471e2157b002ed627231337d
Ruben <nxtzeus4ever@gmail.com>
2024-02-28 11:54:22 +0100

    hola 2

--- Practica/settings.py
+++ Practica/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Practica.wsgi.application'
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': dj_database_url.config(        # Replace this value with your local database's connection string.        
+    'default': dj_database_url.config( 
         default='postgresql://postgres:postgres@localhost:5432/mysite',        
         conn_max_age=600    
     )}
